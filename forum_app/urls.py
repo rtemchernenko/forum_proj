dc40330ebd8d8450c6cfd0888261fede97f9cf97
rtemchernenko <atyomchallenge@gmail.com>
2023-09-23 15:54:18 +0400

    Начал работу с html css
    сервер запускает main.html

---
+++ forum_app/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+from .views import OpenMainWeb
+from . import views
+
+urlpatterns = [
+    path('', views.OpenMainWeb),
+]
--- forum_app/views.py
+++ forum_app/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+def OpenMainWeb(request):
+    return render(request, 'forum_app/main.html')
