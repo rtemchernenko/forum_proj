17d5741fe8b6e122f782ab68627ee132e21a8ee1
rtemchernenko <atyomchallenge@gmail.com>
2025-02-13 16:39:56 +0400

    Добавил уникальный SLUG для постов

--- forum_app/models.py
+++ forum_app/models.py
@@ -8,6 +8,14 @@ from django.contrib.auth.models import User
 
 
 # Create your models here.
+def generate_slug(instance, source_field):
+    slug = slugify(getattr(instance, source_field))
+    unique_slug = slug
+    num = 1
+    while instance.__class__.objects.filter(slug=unique_slug).exists():
+        unique_slug = f"{slug}-{num}"
+        num += 1
+    return unique_slug
 
 
 class Forum(models.Model):
